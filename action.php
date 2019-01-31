9bd7aa6f14ef8f1625d7cd4f7e7a1aa32ff281ae
Kitti solomon <40053238+kittisolomon@users.noreply.github.com>
2019-01-31 12:13:22 +0100

    Update action.php

--- action.php
+++ action.php
@@ -1,6 +1,17 @@
- 	<?php
+class Database {
+ 	public $error;
+ 	public	$con = null;
+
+ 	public function __construct(){
+
+ 		$this->con= mysqli_connect("localhost","root","","databasename");
+ 				if(!$this->con){
+ 					die("cannot connect to database".mysqli_error());
+ 					
+ 						}
+ 						}
   
-  function insert_BandDetails($tablename,$fullname,$address,$dob,$gender,$bank_name,$routineno){
+     function insert_BandDetails($tablename,$fullname,$address,$dob,$gender,$bank_name,$routineno){
 
  			$getEmail="";
  		if(isset($_SESSION["email"])){
@@ -20,4 +31,26 @@
  		}
  	}
   
-  ?>
+  }
+
+// initializing the object of the class
+$obj = new Database();
+
+	if(isset($_POST["submit"])){
+ 	$fullname = $_POST['fullname'];
+ 	$address = $_POST['address'];
+ 	$dob = $_POST['dob'];
+ 	$occupation = $_POST['occupation'];
+ 	$gender = $_POST['gender'];
+    $bank_name = $_POST['bank_name'];
+    $routineno = $_POST['routineno'];
+	
+		$obj->insert_BandDetails("bank_details","{$fullname}","{$address}","{$dob}","{$occupation}","{$gender}","{$bank_name}","{$routineno}");
+}
+}
+
+?>
+
+
+
+
